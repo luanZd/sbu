389d5bfa6b8b3451425357b6c8a63a01aac405fe
Luan <161406737+luanZd@users.noreply.github.com>
2025-09-07 21:35:30 -0300

    Update Main.java

--- src/br/universidade/biblio/Main.java
+++ src/br/universidade/biblio/Main.java
@@ -1,8 +1,8 @@
-```
 package br.universidade.biblio;
 
 import java.util.Scanner;
 
+/// @brief Define os diferentes estados possíveis da aplicação.
 enum Estados {
     INDEFINIDO,
     NO_MENU_DE_OPCOES,
@@ -15,18 +15,19 @@ enum Estados {
     ENCERRANDO;
 }
 
+/// @brief Motor da aplicação.
 public class Main {
 
     //=== Atributos.
-    private Biblioteca biblioteca;
-    private Estados estadoAtual;
-    private Scanner scanner;
-    private int opcaoDoUsuario;
+    // private Biblioteca biblioteca;  //!< Principal interface com o sistema.
+    private Estados estadoAtual;    //!< Estado atual da máquina.
+    private Scanner scanner;        //!< Mecanismo de leitura de estrada.
+    private int opcaoDoUsuario;     //!< Opção escolhida no menu de opções.
 
     //=== Construtor.
     public Main() {
 
-        this.biblioteca = new Biblioteca();
+        // this.biblioteca = new Biblioteca();
         this.estadoAtual = Estados.INDEFINIDO;
         this.scanner = new Scanner(System.in);
         this.opcaoDoUsuario = 0;
@@ -34,7 +35,7 @@ public class Main {
     }
 
     //=== Métodos core.
-    /// @brief Lida com os inputs do usuário.
+    /// @brief Responsável por receber as entradas do usuário.
     public void processar() {
 
         if (estadoAtual == Estados.NO_MENU_DE_OPCOES) {
@@ -43,11 +44,13 @@ public class Main {
 
     }
 
-    /// @brief Atualiza estado interno do sistema.
+    /// @brief Responsável por tratar as entradas e atualizar estado interno do sistema.
     public void atualizar() {
 
         if (estadoAtual == Estados.INDEFINIDO) {
             estadoAtual = Estados.NO_MENU_DE_OPCOES;
+            return; // <-- Evita que a validação do estado "NO_MENU_DE_OPCOES"
+                    // seja executada antes da renderização da interface.
         }
 
         if (estadoAtual == Estados.NO_MENU_DE_OPCOES) {
@@ -63,11 +66,14 @@ public class Main {
 
             } else if (/* imprimir relatorio */false) {
 
+            } else if (/* encerrando aplicação */false) {
+
             } else {
                 /* alguma outra coisa */
             }
 
-            return;
+            return; // <-- Evita que a validação do estado das transições acima
+                    // seja executada antes da renderização da interface.
         }
 
         if (estadoAtual == Estados.EM_ERRO) {
@@ -96,7 +102,7 @@ public class Main {
 
     }
 
-    /// @brief Renderiza interfaces.
+    /// @brief Responsável por desenhar e atualizar os elementos da interface gráfica.
     public void renderizar() {
 
         if (estadoAtual == Estados.NO_MENU_DE_OPCOES) {
@@ -151,4 +157,3 @@ public class Main {
     }
 
 }
-```
